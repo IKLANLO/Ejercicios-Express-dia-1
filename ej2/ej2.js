c54743c99c515500d75b0f39934f7774c094e897
IKLANLO <ilandaberea@yahoo.es>
2023-11-28 18:41:30 +0100

    ejercicio 2 con postman collection

--- ej2/ej2.js
+++ ej2/ej2.js
@@ -2,6 +2,43 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+
+app.get('/', (req, res) => {
+  res.send('Bienvenido!')
+})
+
+app.get('/Productos', (req, res) => {
+  res.send('listado de productos')
+})
+
+app.post('/Productos', (req, res) => {
+  res.send('crear un producto')
+})
+
+app.put('/Productos', (req, res) => {
+  res.send('actualizar un producto')
+})
+
+app.delete('/Productos', (req, res) => {
+  res.send('borrar un producto')
+})
+
+app.get('/Usuarios', (req, res) => {
+  res.send('listado de usuarios')
+})
+
+app.post('/Usuarios', (req, res) => {
+  res.send('crear un usuario')
+})
+
+app.put('/Usuarios', (req, res) => {
+  res.send('actualizar un usuario')
+})
+
+app.delete('/Usuarios', (req, res) => {
+  res.send('borrar un usuario')
+})
+
 app.listen(PORT, () => {
   console.log(`Servidor levantado en el puerto ${PORT}`);
 })
